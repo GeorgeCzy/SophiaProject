ff01b7e32f14e52601635eb123b804ce350dd032
ChengZiyu <chengzy2023@shanghaitech.edu.cn>
2026-08-30 17:15:06 +0800

    Update Google Sheets submit endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SOPHIA_RATING_CONFIG = {
-  submitEndpoint: "https://script.google.com/macros/s/AKfycbzyv0yOdKmduYD3qAW1dpO5QCLwxmYsnUkoJhwjRMLLoLHHKVdnB7-gPdfxybNVOAXkTw/exec",
+  submitEndpoint: "https://script.google.com/macros/s/AKfycbwHpZJWcSgFZ5L7wtFQWk7m0A2SGYXBX1iCuet8MU9MIKXrv2kr8GwYoBbFdgtnKwbw5A/exec",
   submitMode: "no-cors",
 };
